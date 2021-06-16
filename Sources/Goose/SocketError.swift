e8ac92b4dc0cabf68432aed924ef9f00c617e81f
Color <alloy.apple@gmail.com>
2021-06-17 07:07:35 +0800

    修改默认的异常

--- Sources/Goose/SocketError.swift
+++ Sources/Goose/SocketError.swift
@@ -1,43 +1,17 @@
 import Glibc
 
 
-
-protocol BaseGooseError: Error {
-    var msg: String { get  }
-    init(msg: String)
-
-    static func error(
-        file: String, line: Int, func: String
-    ) -> Self
-
-}
-
-extension BaseGooseError {
-    public static func error(
-        file: String = #file, line: Int = #line, func: String = #function
-    ) -> Self {
-        let error = String(cString: strerror(errno))
-        let msg = "\(file):\(line) \(`func`):\(error)"
-        return Self(msg: msg)
-    }
-}
-
-public enum GooseError: Error {
-
-    case message(msg: String)
+public struct GooseError: Error {
+    let name: String
+    let msg: String
+    let code: Int32
 
     public static func error(
-        file: String = #file, line: Int = #line, func: String = #function
+        file: String = #file, line: Int = #line, func: String = #function, name: String = #function, code: Int32 = errno
     ) -> GooseError {
         let error = String(cString: strerror(errno))
         let msg = "\(file):\(line) \(`func`):\(error)"
-        return GooseError.message(msg: msg)
+        return GooseError(name: name,  msg: msg, code:code)
     }
 }
 
-public struct BindError: BaseGooseError {
-    var msg: String
-    init(msg: String) {
-        self.msg = msg
-    }
-}
